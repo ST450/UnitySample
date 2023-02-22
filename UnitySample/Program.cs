d3c8628c8a0df663cf0999288780e85d57e68911
ST450 <ota@ocha458.net>
2023-02-22 15:12:05 +0900

    プロジェクト ファイルを追加します。

---
+++ UnitySample/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+using Unity.Injection;
+
+namespace UnitySample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IUnityContainer container = new UnityContainer();
+
+            // 同じ型に対して、複数の型情報を登録するため名前付きにする
+            container.RegisterType<IVehicle, Car>(nameof(Car));
+            container.RegisterType<IVehicle, Bike>(nameof(Bike));
+            container.RegisterType<CarDriver>(
+                new InjectionConstructor(
+                    new ResolvedParameter<IVehicle>(nameof(Car))
+                    )
+                );
+            container.RegisterType<BikeDriver>(
+                new InjectionConstructor(
+                    new ResolvedParameter<IVehicle>(nameof(Bike))
+                )
+            );
+
+            var list = container.Registrations
+                .Where(r => r.RegisteredType.Name == nameof(IVehicle));
+            foreach (var item in list)
+            {
+                Console.WriteLine($"RegType:{item.RegisteredType.Name} Name:{item.Name}");
+            }
+
+            //var names = new string[] { nameof(Car) , nameof(Bike)};
+            var names = list.Select(r => r.Name);
+            foreach (var name in names)
+            {
+                var obj = container.Resolve<IVehicle>(name);
+                obj.Run();
+                Console.WriteLine($"Run {obj.GetType().Name} - {obj.Run()} mile");
+            }
+
+            var tmpCar = container.Resolve<IVehicle>(nameof(Car));
+            var tmpBile = container.Resolve<IVehicle>(nameof(Bike));
+
+            if (container.Resolve<IVehicle>(nameof(Car)) is Car)
+            {
+                Console.WriteLine("Return Car");
+            }
+            if (container.Resolve<IVehicle>(nameof(Bike)) is Bike)
+            {
+                Console.WriteLine("Return Bike");
+            }
+
+            var carDriver = container.Resolve<CarDriver>();
+            if (carDriver is CarDriver)
+            {
+                carDriver.Run();
+            }
+            var bikeDriver = container.Resolve<BikeDriver>();
+            if (bikeDriver is BikeDriver)
+            {
+                bikeDriver.Run();
+            }
+        }
+    }
+
+    public interface IVehicle
+    {
+        int Run();
+    }
+
+    public class Car : IVehicle
+    {
+        private int _miles = 0;
+        public int Run()
+        {
+            return ++_miles;
+        }
+    }
+
+    public class Bike : IVehicle
+    {
+        private int _miles = 0;
+
+        public int Run()
+        {
+            return ++_miles;
+        }
+    }
+
+    public abstract class Driver
+    {
+        protected IVehicle Vehicle = null;
+        public void Run()
+        {
+            Console.WriteLine($"Run {Vehicle.GetType().Name} - {Vehicle.Run()} mile");
+        }
+    }
+
+    public class CarDriver : Driver
+    {
+        [InjectionConstructor]
+        public CarDriver(IVehicle vehicle)
+        {
+            this.Vehicle = vehicle;
+        }
+    }
+
+    public class BikeDriver : Driver
+    {
+        [InjectionConstructor]
+        public BikeDriver(IVehicle vehicle)
+        {
+            this.Vehicle = vehicle;
+        }
+    }
+}
---
+++ UnityVOSample2/Program.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+using Unity.Injection;
+using UnityVOSample2;
+
+namespace UnityVOSample2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IUnityContainer container = new UnityContainer();
+
+            // 同じ型に対して、複数の型情報を登録するため名前付きにする
+            container.RegisterType<IValueObjectBuilder, ValueObjectDriver1>(nameof(ValueObjectDriver1));
+            container.RegisterType<IValueObjectBuilder, ValueObjectDriver2>(nameof(ValueObjectDriver2));
+
+            // 実際に使用するValueObjectクラスと対応付けできるよう関数を作成
+            void ValueObjectBuilderRegisterType(Type type, string name)
+            {
+                //var injection =
+                //    new InjectionConstructor(new ResolvedParameter<IValueObjectBuilder>(name));
+                //container.RegisterType(type, injection);
+
+                var inj = new InjectionMethod("SetValueObjectBuilder", new ResolvedParameter<IValueObjectBuilder>(name));
+                container.RegisterType(type, inj);
+
+                // 下でエラーになる 引数なしのコンストラクタがないため
+                var obj = container.Resolve(type, null);
+            }
+
+            ValueObjectBuilderRegisterType(typeof(VCode1), nameof(ValueObjectDriver1));
+            ValueObjectBuilderRegisterType(typeof(VCode2), nameof(ValueObjectDriver2));
+
+            {
+                var vo1 = new VCode1("1");
+                vo1.Print();
+                var vo2 = new VCode2("A");
+                vo2.Print();
+            }
+            {
+                var vo1 = new VCode1("2");
+                vo1.Print();
+                var vo2 = new VCode2("B");
+                vo2.Print();
+            }
+            Console.ReadLine();
+        }
+    }
+
+    public abstract class ValueObject<T> where T : ValueObject<T>
+    {
+        protected static IValueObjectBuilder _valueObjectBuilder;
+
+        [InjectionMethod]
+        public void SetValueObjectBuilder(IValueObjectBuilder valueObjectBuilder)
+        {
+            _valueObjectBuilder = valueObjectBuilder;
+
+        }
+
+        public override string ToString()
+        {
+            return ToStringCore();
+        }
+
+        public override bool Equals(object obj)
+        {
+            var vo = obj as T;
+            if (vo == null)
+            {
+                return false;
+            }
+
+            return EqualsCore(vo);
+        }
+
+        public override int GetHashCode()
+        {
+            return GetHashCodeCore();
+        }
+
+        public static bool operator ==(ValueObject<T> vo1, ValueObject<T> vol2)
+        {
+            return Equals(vo1, vol2);
+        }
+
+        public static bool operator !=(ValueObject<T> vo1, ValueObject<T> vol2)
+        {
+            return !(vo1 == vol2);
+        }
+
+        protected abstract bool EqualsCore(T other);
+
+        protected abstract int GetHashCodeCore();
+
+        protected abstract string ToStringCore();
+    }
+
+
+    public interface IValueObjectBuilder
+    {
+        IEnumerable<object> GetData();
+    }
+
+    public class CodeName1
+    {
+        public int code;
+        public string name;
+    }
+
+    public class ValueObjectDriver1 : IValueObjectBuilder
+    {
+        public IEnumerable<object> GetData()
+        {
+            yield return new CodeName1 {code = 1, name = "name1"};
+            yield return new CodeName1 {code = 2, name = "name2"};
+        }
+    }
+
+    public class VCode1 : ValueObject<VCode1>
+    {
+        public string Value { get; }
+
+        //private static IValueObjectBuilder _valueObjectBuilder;
+        private static Dictionary<int, string> _dic;
+
+        public VCode1(string value)
+        {
+            Value = value;
+            if (_dic == null)
+            {
+                _dic = _valueObjectBuilder.GetData()
+                    .OfType<CodeName1>()
+                    .ToDictionary(r => r.code, r => r.name);
+                Console.WriteLine("VCode1 Dic作成");
+            }
+        }
+
+        //[InjectionConstructor]
+        public VCode1(IValueObjectBuilder valueObjectBuilder)
+        {
+            _valueObjectBuilder = valueObjectBuilder;
+            Console.WriteLine($"VCode1 InjectionConstructor ");
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"VCode1 Value:{Value}");
+            foreach (var m in _dic)
+            {
+                Console.WriteLine($"code:{m.Key} name:{m.Value}");
+            }
+
+        }
+
+        protected override bool EqualsCore(VCode1 other)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override int GetHashCodeCore()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override string ToStringCore()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class CodeName2
+    {
+        public string code;
+        public string name;
+    }
+
+    public class ValueObjectDriver2 : IValueObjectBuilder
+    {
+        public IEnumerable<object> GetData()
+        {
+            yield return new CodeName2 {code = "a", name = "namea"};
+            yield return new CodeName2 {code = "b", name = "nameb"};
+        }
+    }
+
+    public class VCode2 : ValueObject<VCode2>
+    {
+        public string Value { get; }
+
+        //private static IValueObjectBuilder _valueObjectBuilder;
+        private static Dictionary<string, string> _dic;
+
+        public VCode2(string value)
+        {
+            Value = value;
+            if (_dic == null)
+            {
+                _dic = _valueObjectBuilder.GetData()
+                    .OfType<CodeName2>()
+                    .ToDictionary(r => r.code, r => r.name);
+                Console.WriteLine("VCode2 Dic作成");
+            }
+        }
+
+        //[InjectionConstructor]
+        public VCode2(IValueObjectBuilder valueObjectBuilder)
+        {
+            _valueObjectBuilder = valueObjectBuilder;
+            Console.WriteLine($"VCode2 InjectionConstructor ");
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"VCode2 Value:{Value}");
+            foreach (var m in _dic)
+            {
+                Console.WriteLine($"code:{m.Key} name:{m.Value}");
+            }
+
+        }
+
+        protected override bool EqualsCore(VCode2 other)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override int GetHashCodeCore()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override string ToStringCore()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
